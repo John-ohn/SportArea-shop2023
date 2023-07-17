4d03ad2491b3ddb5309f4bb0e401c90983d265ed
Lyubomyr <bobboby403@gmail.com>
2023-07-17 19:44:52 +0300

    create app Sport Area ver.1

---
+++ Back end /sportArea/src/main/java/com/sportArea/SportAreaApplication.java
@@ -0,0 +1,13 @@
+package com.sportArea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportAreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportAreaApplication.class, args);
+	}
+
+}
